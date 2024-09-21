9c71f06acfa8891ab1626542f7601eece053e0b3
eagarcia77 <131388877+eagarcia77@users.noreply.github.com>
2024-09-21 19:42:02 -0400

    Add files via upload

---
+++ descarga_agent.py
@@ -0,0 +1,5 @@
+
+# Este es un ejemplo de agente que puede ser descargado desde la web.
+# En la versión completa, este script realizará auditorías en el sistema local.
+
+print("Este es el agente de auditoría. En una versión completa, ejecutará la auditoría de la computadora.")
